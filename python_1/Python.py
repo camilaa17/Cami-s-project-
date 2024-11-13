61640db0bf3a736b19ba670f0c4aba6905972442
camilaa17 <camilaa17>
2024-11-13 17:51:37 -0500

    Finished submiting my flowchart to schoology

---
+++ python_1/Python.py
@@ -0,0 +1,12 @@
+
+def addNumbers( firstNumber, secondNumber):
+    sum = firstNumber + secondNumber
+    print( sum )
+
+addNumbers( 7, 9 )
+addNumbers( 2,4)
+
+userFirstNumber = input("What's your first number:")
+userSecondNumber = input("What's your second number:")
+
+addNumbers( userFirstNumber, userSecondNumber )
