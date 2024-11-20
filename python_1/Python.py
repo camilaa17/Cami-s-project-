a54af1328315982733aeb52b86fc4a986ed96442
camilaa17 <camilaa17>
2024-11-20 17:49:38 -0500

    Finished and edited my python code

--- python_1/Python.py
+++ python_1/Python.py
@@ -1,4 +1,5 @@
 
+# This function adds 2 numbers
 def addNumbers( firstNumber, secondNumber):
     sum = firstNumber + secondNumber
     print( sum )
@@ -10,3 +11,29 @@ userFirstNumber = input("What's your first number:")
 userSecondNumber = input("What's your second number:")
 
 addNumbers( userFirstNumber, userSecondNumber )
+# Defining a variable 'temperature'
+temperature = 50.5
+if temperature < 30:
+    print("It's cold outside!")
+elif temperature > 50:
+    print("It's hot outside!")
+          
+#Creatiing  a for loop that iterates over a range of numbers from 0 to 4
+for number in range (5):
+    #printing the current value of 'number'
+    print(f"current number is {number}")
+
+#Creating a list of fruits
+fruits_list = ["apple", "banana", "strawberry" ]
+print(f"Fruits list: {fruits_list}")
+
+second_fruit = fruits_list[1]
+print(f"The second fruit in the list is {second_fruit}")
+
+colora_tuple = ("red", "green", "blue")
+print(f"Colors tuple: {colora_tuple}")
+
+
+# Need At least 1 iterated list with accessed and used elements in it.
+for eachColoraTuple in colora_tuple:
+    print( eachColoraTuple )
