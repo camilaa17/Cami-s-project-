a83b15e054474b07f39a7e66573f6c534a594176
camilaa17 <camilaa17>
2025-02-19 18:07:07 -0500

    Work more on javascript

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -6,3 +6,7 @@ let age = 16;
 const birthYear = 2008 
 
 console.log("My age is ", age)
+// Creating an if statement
+if (age <= 16) {
+    console.log('You are not an adult'); // This block runs if age is 16 or younger
+}
